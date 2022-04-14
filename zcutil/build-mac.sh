76c7db08e439e882c8f8727f0dd9b975a8b9c1ed
ComputerGenie <tim@yourcomputergenie.com>
2022-04-14 15:31:55 -0500

    Update build-mac.sh

--- zcutil/build-mac.sh
+++ zcutil/build-mac.sh
@@ -1,9 +1,58 @@
-#!/usr/bin/env bash
+#!/bin/bash
+export CC=gcc-8
+export CXX=g++-8
+export LIBTOOL=libtool
+export AR=ar
+export RANLIB=ranlib
+export STRIP=strip
+export OTOOL=otool
+export NM=nm
 
-# seems clang on native darwin doesn't support -fopenmp compiler flag (MULTICORE=1 for libsnark)
-sed -i.old -e 's|\(CURVE=ALT_BN128[ \t]*MULTICORE=\)\([0-9]\{1,\}\)|\10|' ./depends/packages/libsnark.mk
-make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=x86_64-apple-darwin18 -j$(nproc --all)
+mydir="$PWD"
+set -eu -o pipefail
+
+if [ "x$*" = 'x--help' ]
+then
+    cat <<EOF
+Usage:
+
+$0 --help
+  Show this help message and exit.
+
+$0 [ --enable-lcov ] [ MAKEARGS... ]
+  Build Zcash and most of its transitive dependencies from
+  source. MAKEARGS are applied to both dependencies and Zcash itself. If
+  --enable-lcov is passed, Zcash is configured to add coverage
+  instrumentation, thus enabling "make cov" to work.
+EOF
+    exit 0
+fi
+
+# If --enable-lcov is the first argument, enable lcov coverage support:
+LCOV_ARG=''
+HARDENING_ARG='--disable-hardening'
+if [ "x${1:-}" = 'x--enable-lcov' ]
+then
+    LCOV_ARG='--enable-lcov'
+    HARDENING_ARG='--disable-hardening'
+    shift
+fi
+
+TRIPLET=`./depends/config.guess`
+PREFIX="$(pwd)/depends/$TRIPLET"
+
+make "$@" -C ./depends/ V=1 # NO_PROTON=1
+
+
 ./autogen.sh
-CXXFLAGS="-g0 -O2" \
-CONFIG_SITE="$PWD/depends/x86_64-apple-darwin18/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
-make V=1 -j$(nproc --all)
+
+LDFLAGS="-static-libgcc -static-libstdc++"
+CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
+
+# -Werror should be removed from CPPFLAGS, othewise Qt static plugins determine on ./configure
+# step will cause an error and static Qt plugins will not be linked.
+
+CXXFLAGS='-arch x86_64 -I/usr/local/Cellar/gcc\@8/8.3.0/include/c++/8.3.0/ '"-I${PREFIX}/include"' -fwrapv -fno-strict-aliasing -g0 -O2 -Wl,-undefined -Wl,dynamic_lookup' \
+./configure --prefix="${PREFIX}" --disable-bip70 --with-gui=qt5 --enable-tests=no "$HARDENING_ARG" "$LCOV_ARG"
+
+make "$@" V=1 NO_GTEST=1 STATIC=1
