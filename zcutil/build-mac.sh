27736f02307198add2f2f5a0c7d4c35423a12b44
ComputerGenie <tim@yourcomputergenie.com>
2022-04-18 10:48:31 -0500

    Update build-mac.sh

--- zcutil/build-mac.sh
+++ zcutil/build-mac.sh
@@ -1,24 +1,5 @@
-#!/bin/bash
-export CC=gcc-8
-export CXX=g++-8
-export LIBTOOL=libtool
-export AR=ar
-export RANLIB=ranlib
-export STRIP=strip
-export OTOOL=otool
-export NM=nm
-
-
-
-TRIPLET=`./depends/config.guess`
-PREFIX="$(pwd)/depends/$TRIPLET"
-
-make "$@" -C ./depends/ V=1 NO_QT=1 NO_PROTON=1
-
+make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=x86_64-apple-darwin20 -j$(nproc --all)
 ./autogen.sh
-LDFLAGS="-static-libgcc -static-libstdc++"
-CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
-CXXFLAGS='-arch x86_64 -I/usr/local/Cellar/gcc\@8/8.3.0/include/c++/8.3.0/ '"-I${PREFIX}/include"' -fwrapv -fno-strict-aliasing -g0 -O2 -Wl,-undefined -Wl,dynamic_lookup' \
-./configure --prefix="${PREFIX}" --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
-
-make "$@" V=1 NO_GTEST=1 STATIC=1
+CXXFLAGS="-g0 -O2" \
+CONFIG_SITE="$PWD/depends/x86_64-apple-darwin20/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
+make V=1 -j$(nproc --all)
