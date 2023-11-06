f7e1edcf4f8baf7c0e2d3e51567798d7218844c0
DeckerSU <deckersu@protonmail.com>
2023-11-06 20:57:17 +0100

    tests: KomodoFeatures fix missed override keyword for member- functions

--- src/test-komodo/test_kmd_feat.cpp
+++ src/test-komodo/test_kmd_feat.cpp
@@ -35,7 +35,7 @@ public:
         sAllowedTxIn.insert(uint256S("3533600e69a22776afb765305a0ec46bcb06e1942f36a113d73733190092f9d5")); // 10 * COIN, nLockTime = 1663755147
     }
 
-    bool GetCoins(const uint256 &txid, CCoins &coins) const {
+    bool GetCoins(const uint256 &txid, CCoins &coins) const override {
         if (sAllowedTxIn.count(txid)) {
             CTxOut txOut;
             txOut.nValue = 10 * COIN;
@@ -50,7 +50,7 @@ public:
         return false;
     }
 
-    bool HaveCoins(const uint256 &txid) const {
+    bool HaveCoins(const uint256 &txid) const override {
         if (sAllowedTxIn.count(txid))
             return true;
         return false;
@@ -68,11 +68,11 @@ public:
                     CAnchorsSproutMap &mapSproutAnchors,
                     CAnchorsSaplingMap &mapSaplingAnchors,
                     CNullifiersMap &mapSproutNullifiers,
-                    CNullifiersMap &mapSaplingNullifiers) {
+                    CNullifiersMap &mapSaplingNullifiers) override {
         return false;
     }
 
-    bool GetStats(CCoinsStats &stats) const {
+    bool GetStats(CCoinsStats &stats) const override {
         return false;
     }
 
