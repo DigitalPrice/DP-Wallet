9e4a04be0149c6479db51d29e276726cae5abe72
ComputerGenie <tim@yourcomputergenie.com>
2024-01-23 09:47:51 -0600

    tidy packages

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,21 +1,3 @@
-rust_packages := rust librustzcash
-
-ifeq ($(build_os),darwin)
-zcash_packages := libsnark libgmp libsodium
-else
-	zcash_packages := libgmp libsodium
-endif
-
-qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf
-
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
-qt_android_packages=qt
-
-qt_darwin_packages=qt
-qt_mingw32_packages=qt
-
-
 rust_crates := \
   crate_aes \
   crate_aesni \
@@ -46,24 +28,42 @@ rust_crates := \
   crate_opaque_debug \
   crate_rand \
   crate_stream_cipher \
-  crate_typenum \
-  crate_winapi_i686_pc_windows_gnu \
-  crate_winapi \
-  crate_winapi_x86_64_pc_windows_gnu
+  crate_typenum
+  
+ifeq ($(host_os),mingw32)
+	rust_crates += \
+	  crate_winapi_i686_pc_windows_gnu \
+	  crate_winapi \
+	  crate_winapi_x86_64_pc_windows_gnu
+endif
 
-rust_packages := rust $(rust_crates) librustzcash
-native_packages := native_ccache
+ifeq ($(build_os),darwin)
+	zcash_packages := libsnark libgmp libsodium
+else
+	zcash_packages := libgmp libsodium
+endif
 
-wallet_packages=bdb
+qt_linux_packages := expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
+qt_native_packages := native_protobuf
+qt_packages := qt qrencode protobuf
 
 packages := boost openssl libevent zeromq $(zcash_packages) googletest libcurl # googlemock
 
+rust_packages := rust $(rust_crates) librustzcash
+
+native_packages := native_ccache
+
+wallet_packages := bdb
+
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_libtapi native_cdrkit
-ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-darwin_native_packages+= native_clang
+	darwin_native_packages += native_cctools native_libtapi native_cdrkit
+	ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+		darwin_native_packages += native_clang
+	endif
+	ifeq ($(host_os),darwin)
+		packages += libsnark
+	endif
 endif
-ifeq ($(host_os),darwin)
-packages += libsnark
-endif
-endif
+
+# all non-appending assignments should be simple
+# qt assignments should be rolled into the non-qt assignments in a qt-only repo; but, that's for later
